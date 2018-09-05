1ae577faf34f2837c24f5f66d2fa064fec929939
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-05 16:11:01 +0530

    Update and rename README.md to roundoff.java

---
+++ roundoff.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	System.out.println("23.3 afterrounding:"+Math.round(23.2));
+		System.out.println("16.5 afterrounding:"+Math.round(16.5));
+	System.out.println("35.8 afterrounding:"+Math.round(35.8));
+
+	}
+}
